6858bb7d3d570d04da37d27bffe0637b5429d2ca
judas-christ <daniel.hagglund+github@gmail.com>
2012-12-14 09:38:20 +0100

    Updated documentation with licensse and usage examples. Fix for when inputs have no id.

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-placeholder2000
-===============
+# placeholder2000
 
 A quick and dirty placeholder plugin for jQuery.
 
 * Works in IE8, IE9, Chrome etc
 * Works with jQuery validate and jQuery unobtrusive validation
+
+## Usage
+
+Simple usage:
+
+    <script>
+	    $(function() {
+	        $('input[placeholder]').placeholder();
+        });
+	</script>
+	
+With options:
+
+    <script>
+	    $(function() {
+	        $('input[placeholder]').placeholder({ cssClass: 'myPlaceHolderClass', idPrefix: 'myInput' });
+        });
+	</script>
+	
+### Options
+
+*cssClass*	-	CSS class of placeholder
+*idPrefix*	-	prefix for generated IDs when missing on the inputs
+
+## License
+
+MIT license - http://www.opensource.org/licenses/mit-license.php
--- placeholder2000.js
+++ placeholder2000.js
@@ -3,18 +3,28 @@ Placeholder 2000
 https://github.com/judas-christ/placeholder2000
 */
 (function (window, document, $) {
+	'use strict';
     var DATA_KEY = 'ph2k';
     var NATIVE_SUPPORT = 'placeholder' in document.createElement('input');
-    function createPlaceholder(input) {
+	var DEFAULT_OPTIONS = { cssClass: 'placeholder', idPrefix: 'placeholder-input-' };
+	var index = 0;
+    function createPlaceholder(input, options) {
         var placeholder = input.data(DATA_KEY);
         if(!placeholder) {
-            placeholder = new Placeholder(input);
+            placeholder = new Placeholder(input, options);
             input.data(DATA_KEY, placeholder);
         }
         return placeholder;
     }
-    function Placeholder(input) {
-        this.label = $('<label />').attr({'for':input.attr('id'),'class':'placeholder'}).text(input.attr('placeholder'));
+    function Placeholder(input, options) {
+		var id = input.attr('id'); 
+		if(!id) {
+			id = options.idPrefix + (index++);
+			input.attr('id', id);
+		}
+        this.label = $('<label />')
+			.attr({ 'for': id, 'class': options.cssClass })
+			.text(input.attr('placeholder'));
         this.input = input;
         input.before(this.label);
     }
@@ -27,10 +37,11 @@ https://github.com/judas-christ/placeholder2000
             this.label.hide();
         }
     };
-    $.fn.placeholder = function() {
+    $.fn.placeholder = function(options) {
+		options = $.extend({}, DEFAULT_OPTIONS, options);
         return NATIVE_SUPPORT ? this : this.each(function(i, element) {
             var input = $(element);
-            var placeholder = createPlaceholder(input);
+            var placeholder = createPlaceholder(input, options);
             placeholder.show();
             input.focus(function() {
                 placeholder.hide();
